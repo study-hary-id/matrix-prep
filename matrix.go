822bb13e2bc12998d6e406556c8d273ddf2261f1
study-hary-id <haryansyah.study.id@gmail.com>
2021-02-02 14:41:44 +0700

    Fix Matrix function bugs Rename package to 'matrixprep' Added description to the function Improve multidimensional array for matrix

--- matrix.go
+++ matrix.go
@@ -1,7 +1,15 @@
-package matrix-prep
+package matrixprep
 
-func Matrix(row, col) []int  {
-	matrix := make([]int, row, 99)
+/*
+Matrix is used to create matrix based on
+row and column which has been provided
+*/
+func Matrix(row int8, col int8) [][]int8 {
+	matrix := make([][]int8, row)
+
+	for i := range matrix {
+		matrix[i] = make([]int8, col)
+	}
+
 	return matrix
 }
-
