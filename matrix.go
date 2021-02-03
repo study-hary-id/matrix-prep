6f739cedc67ce553f03ebb773a475b79ea05909f
study-hary-id <haryansyah.study.id@gmail.com>
2021-02-03 19:59:16 +0700

    Update module 'matrix.go' Improve 'Matrix()' description Added 'Visual()' and its description

--- matrix.go
+++ matrix.go
@@ -1,8 +1,11 @@
 package matrixprep
 
+import "strconv"
+
 /*
-Matrix is used to create matrix based on
-row and column which has been provided
+Matrix is used to create matrix based on row and column
+which has been provided. The matrix is a multidimensional
+slice with int8 type.
 */
 func Matrix(row int8, col int8) [][]int8 {
 	matrix := make([][]int8, row)
@@ -12,4 +15,26 @@ func Matrix(row int8, col int8) [][]int8 {
 	}
 
 	return matrix
+}
+
+/*
+Visual is used to visualize matrix with return type of string.
+*/
+func Visual(matrix [][]int8) string {
+	var visualMatrix string
+
+	for i, v := range matrix {
+		visualMatrix += "| "
+		for _, subv := range v {
+			visualMatrix += strconv.Itoa(int(subv))
+			visualMatrix += " "
+		}
+
+		visualMatrix += "|"
+		if i < len(matrix)-1 {
+			visualMatrix += "\n"
+		}
+	}
+
+	return visualMatrix
 }
